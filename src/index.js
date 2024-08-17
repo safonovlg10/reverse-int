fa882d9f0c5bc502117d254427be4fcbb7f630c1
safonovlg10 <safonovlg10@gmail.com>
2024-08-17 18:37:10 +0900

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    return Math.sign(n) != -1 ? Number(n.toString().split('').reverse().join('')) : Number(n.toString().split('').map((item, index, arr) =>{
+        return arr[arr.length - index];
+      }).join(''));
 }
